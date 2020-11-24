f5aaed7b83378c0e93b19c93199775118263f75c
abeltefera <abeltefera16@gmail.com>
2020-11-24 14:22:21 +0300

    server configured

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+connectDB();
+
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.json({ msg: "ContactsKeeper Api" }));
 
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
-
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
 const PORT = process.env.PORT || 5000;
 
