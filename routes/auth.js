6a49de776f885aee593ecc6b26554a3f1466fd8e
abeltefera <abeltefera16@gmail.com>
2020-11-24 15:06:01 +0300

    auth middleware set

--- routes/auth.js
+++ routes/auth.js
@@ -4,10 +4,17 @@ const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
 const jwt = require("jsonwebtoken");
 const config = require("config");
+const auth = require("../middleware/auth");
 const User = require("../models/User");
 
-router.get("/", (req, res) => {
-  res.send("Get logged in user");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 router.post(
