bfa4c2922047262b6e6201994745377bc54e6397
abeltefera <abeltefera16@gmail.com>
2020-12-01 20:49:22 +0300

    CRUD backend integration

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -2,37 +2,69 @@ import React, { Fragment, useContext } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import AuthContext from "../../context/auth/authContext";
+import ContactContext from "../../context/contact/contactContext";
 
-const Navbar = ({ title, icon }) => {
+const Navbar = (props) => {
   const authContext = useContext(AuthContext);
-  const { isAuthenticated } = authContext;
+  const contactContext = useContext(ContactContext);
+
+  const { isAuthenticated, logout, user } = authContext;
+  
+  const onLogout = () => {
+    logout();
+    contactContext.clearContacts();
+    // props.history.push("/login");
+  };
+
+  const authLinks = (
+    <Fragment>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <a onClick={onLogout}>
+          <i className="fas fa-sign-out-alt"></i>{" "}
+          <span className="hide-sm">Logout</span>
+        </a>
+      </li>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </Fragment>
+  );
 
   return (
     <div className="navbar bg-primary">
-      <h1>
-        <i className={icon} /> {title}
-      </h1>
       <ul>
-        {isAuthenticated ? (
-          <Fragment>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </Fragment>
-        ) : (
-          <Fragment>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </Fragment>
-        )}
+        <li style={{ display: "block" }}>
+          <h1>
+            <i className={props.icon} /> {props.title}
+          </h1>
+          {user && (
+            <h4
+              style={{
+                marginLeft: "3rem",
+                lineHeight: "0",
+                marginBottom: "1rem",
+              }}
+            >
+              Hello, {user.name}
+            </h4>
+          )}
+        </li>
       </ul>
+      <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
     </div>
   );
 };
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,55 +1,76 @@
 import React, { useReducer } from "react";
-import uuid from "uuid";
 import ContactContext from "./contactContext";
 import contactReducer from "./contactReducer";
+import axios from "axios";
 import {
   ADD_CONTACT,
   DELETE_CONTACT,
+  GET_CONTACTS,
+  CLEAR_CONTACTS,
+  SET_LOADING,
   SET_CURRENT,
   CLEAR_CURRENT,
   UPDATE_CONTACT,
   FILTER_CONTACTS,
   CLEAR_FILTER,
+  CONTACT_ERROR,
 } from "../types";
 
 const ContactState = (props) => {
   const initialState = {
-    contacts: [
-      {
-        id: uuid.v4(),
-        name: "Dutch",
-        email: "goddamnplan@gmail.com",
-        phone: "111-11-1",
-        type: "personal",
-      },
-      {
-        id: uuid.v4(),
-        name: "John",
-        email: "johnjim@gmail.com",
-        phone: "222-22-2",
-        type: "professional",
-      },
-      {
-        id: uuid.v4(),
-        name: "Micah",
-        email: "rat@gmail.com",
-        phone: "666-66-6",
-        type: "personal",
-      },
-    ],
+    contacts: null,
     current: null,
-    filtered: null
+    filtered: null,
+    error: null,
+    loading: false,
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
-  const addContact = (contact) => {
-    contact.id = uuid.v4();
-    dispatch({ type: ADD_CONTACT, payload: contact });
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
   };
 
-  const deleteContact = (id) => {
-    dispatch({ type: DELETE_CONTACT, payload: id });
+  const addContact = async (contact) => {
+    // contact.id = uuid.v4();
+    dispatch({ type: SET_LOADING, payload: true });
+
+    try {
+      const res = await axios.post("/api/contacts", contact, config);
+      dispatch({ type: ADD_CONTACT, payload: res.data });
+    } catch (error) {
+      dispatch({ type: CONTACT_ERROR, payload: error.response.msg });
+    } finally {
+      dispatch({ type: SET_LOADING, payload: false });
+    }
+  };
+
+  const getContacts = async () => {
+    dispatch({ type: SET_LOADING, payload: true });
+
+    try {
+      const res = await axios.get("/api/contacts");
+      dispatch({ type: GET_CONTACTS, payload: res.data });
+    } catch (error) {
+      dispatch({ type: CONTACT_ERROR, payload: error.response.msg });
+    } finally {
+      dispatch({ type: SET_LOADING, payload: false });
+    }
+  };
+
+  const deleteContact = async (id) => {
+    dispatch({ type: SET_LOADING, payload: true });
+
+    try {
+      await axios.delete(`/api/contacts/${id}`);
+      dispatch({ type: DELETE_CONTACT, payload: id });
+    } catch (error) {
+      dispatch({ type: CONTACT_ERROR, payload: error.response.msg });
+    } finally {
+      dispatch({ type: SET_LOADING, payload: false });
+    }
   };
 
   const setCurrent = (contact) => {
@@ -60,16 +81,33 @@ const ContactState = (props) => {
     dispatch({ type: CLEAR_CURRENT });
   };
 
-  const updateContact = (contact) => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  const updateContact = async (contact) => {
+    dispatch({ type: SET_LOADING, payload: true });
+
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+      dispatch({ type: UPDATE_CONTACT, payload: res.data });
+    } catch (error) {
+      dispatch({ type: CONTACT_ERROR, payload: error.response.msg });
+    } finally {
+      dispatch({ type: SET_LOADING, payload: false });
+    }
   };
 
-  const filterContacts = text => {
-    dispatch({type: FILTER_CONTACTS, payload: text})
+  const filterContacts = (text) => {
+    dispatch({ type: FILTER_CONTACTS, payload: text });
   };
 
   const clearFilter = () => {
     dispatch({ type: CLEAR_FILTER });
+  };
+
+  const clearContacts = () => {
+    dispatch({ type: CLEAR_CONTACTS });
   };
 
   return (
@@ -78,13 +116,16 @@ const ContactState = (props) => {
         contacts: state.contacts,
         current: state.current,
         filtered: state.filtered,
+        error: state.error,
         addContact,
         deleteContact,
         setCurrent,
         clearCurrent,
         updateContact,
         filterContacts,
-        clearFilter
+        clearFilter,
+        getContacts,
+        clearContacts,
       }}
     >
       {props.children}
